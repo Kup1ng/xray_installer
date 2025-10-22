36bcfdc65104e62a9e4aa414ae6439d87f57206d
Mahan <not.kup1ng@gmail.com>
2025-10-22 22:59:20 +0330

    Update default.sh

--- default.sh
+++ default.sh
@@ -1,1 +1,1 @@
-curl -4 -s -H -fsSL https://raw.githubusercontent.com/Kup1ng/xray_installer/main/install.sh | bash -s v25.10.15
+curl -4 -s -H -fsSL https://raw.githubusercontent.com/Kup1ng/xray_installer/main/install.sh | bash -s v25.7.25
